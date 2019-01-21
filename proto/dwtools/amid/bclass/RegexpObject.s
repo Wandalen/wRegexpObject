6ef25898fc0982e31486170b771221bc4352fd61
vmigdal <volodymyr.migdal@gmail.com>
2019-01-21 11:07:28 +0200

    adjust doc, upgrade

--- proto/dwtools/amid/bclass/RegexpObject.s
+++ proto/dwtools/amid/bclass/RegexpObject.s
@@ -3,7 +3,8 @@
 'use strict';
 
 /**
-  @module Tools/mid/RegexpObject - Class which encapsulates a trivial logical combination( expression ) and regular expressions which may be applied to a string to tell does that string satisfies regular expressions as well as the logic. RegexpObject provides functionality to compose, combine several instances of the class, extend it, apply to a string and other. Use it to treat multiple conditions as a single object. Refactoring required.
+ * Class which encapsulates a trivial logical combination( expression ) and regular expressions which may be applied to a string to tell does that string satisfies regular expressions as well as the logic. RegexpObject provides functionality to compose, combine several instances of the class, extend it, apply to a string and other. Use it to treat multiple conditions as a single object. Refactoring required.
+  @module Tools/mid/RegexpObject
 */
 
 /**
