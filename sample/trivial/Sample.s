69039defd915029c259a3072b353439c77f0321f
wandalen <wandalen.me@gmail.com>
2021-02-24 08:57:30 +0200

    .

--- sample/trivial/Sample.s
+++ sample/trivial/Sample.s
@@ -5,5 +5,5 @@ let _ = wTools;
 
 var regexpObject = _.RegexpObject( 'abc', 'includeAny' );
 
-console.log( 'regexpObject :\n' + _.toStr( regexpObject ) );
+console.log( 'regexpObject :\n' + _.entity.exportString( regexpObject ) );
 console.log( 'regexpObject.test( "abc" ) :', regexpObject.test( 'abc' ) );
